b1256a4c2becddf39be3e91ab1f0dc64da40e058
Ben Taylor <bentaylor@knights.ucf.edu>
2020-03-19 14:41:18 -0400

    added spotify search functionality, small error with artist name not showing (will attempt fix later)

--- liri.js
+++ liri.js
@@ -28,6 +28,38 @@ var concertForBand = function(artist) {
 };
 
 
+var spotifySearch = function(songName) {
+    if (songName === undefined) {
+      songName = "The Sign";
+    }
+  
+    spotify.search(
+      {
+        type: "track",
+        query: songName
+      },
+      function(err, data) {
+        if (err) {
+          console.log("Error occurred: " + err);
+          return;
+        }
+  
+        var songList = data.tracks.items;
+  
+        for (var i = 0; i < songList.length; i++) {
+          console.log(i);
+          console.log("artist(s): " + songList[i].artists);
+          console.log("song name: " + songList[i].name);
+          console.log("preview song: " + songList[i].preview_url);
+          console.log("album: " + songList[i].album.name);
+          console.log("-------");
+        }
+      }
+    );
+  };
+
 if (process.argv[2] == "concert-this"){
 concertForBand("circa survive");
+} else if (process.argv[2] == "spotify-this-song"){
+spotifySearch("Everyway")
 }
